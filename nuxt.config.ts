a5fd66b175e2ba5796bb60490df4c7d73c0c4ca3
Glenn <dev-glennn@gmail.com>
2023-12-09 17:11:33 +0900

    Chore: [ADD] tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,15 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/reset.css', '~/assets/scss/app.scss'],
+  css: ['~/assets/css/tailwind.css', '~/assets/css/reset.css', '~/assets/scss/app.scss'],
   devtools: { enabled: true },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
 
   modules: [
     '@nuxtjs/eslint-module'
