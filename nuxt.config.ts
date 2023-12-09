5a06f0c96a26ff819810bc5544e62762198dbe3b
Glenn <dev-glennn@gmail.com>
2023-12-09 15:07:36 +0900

    Chore: eslint, prettier 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
+import eslintPlugin from 'vite-plugin-eslint';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/css/reset.css', '~/assets/scss/app.scss'],
   devtools: { enabled: true },
+
+  modules: [
+    '@nuxtjs/eslint-module'
+  ],
+
+  eslint: {
+    // lintOnStart: false
+  },
+
+  vite: {
+    plugins: [
+      eslintPlugin()
+    ]
+  }
 })
